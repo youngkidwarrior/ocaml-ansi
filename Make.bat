30c69121d3bde01a3adfb7486e228231a5e12b60
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2010-11-02 11:32:43 +0000

    Windows: compile ANSITerminal_common.ml.
    git-svn-id: svn+ssh://scm.ocamlcore.org/svnroot/ansiterminal/trunk@28 3e1261b6-7e74-4a22-bca0-71f0aa2b879a

--- Make.bat
+++ Make.bat
@@ -13,10 +13,12 @@ ocamlc -c -I +caml ANSITerminal_win_stubs.c
 
 %OCAMLC% %OCAMLCFLAGS% -c ANSITerminal.mli
 %OCAMLC% %OCAMLCFLAGS% -c ANSITerminal.ml
-%OCAMLC% %OCAMLCFLAGS% -a -o %LIB_BYTE%  ANSITerminal.cmo
+%OCAMLC% %OCAMLCFLAGS% -c ANSITerminal_common.ml
+%OCAMLC% %OCAMLCFLAGS% -a -o %LIB_BYTE%  ANSITerminal_common.cmo ANSITerminal.cmo
 
 %OCAMLOPT% %OCAMLOPTFLAGS% -c ANSITerminal.ml
-%OCAMLOPT% %OCAMLOPTFLAGS% -a -o %LIB_OPT% ANSITerminal.cmx user32.lib ANSITerminal_win_stubs.obj
+%OCAMLOPT% %OCAMLOPTFLAGS% -c ANSITerminal_common.ml
+%OCAMLOPT% %OCAMLOPTFLAGS% -a -o %LIB_OPT% ANSITerminal_common.cmx ANSITerminal.cmx user32.lib ANSITerminal_win_stubs.obj
 
 %OCAMLC% %OCAMLCFLAGS% -o showcolors.exe %LIB_BYTE% showcolors.ml
 %OCAMLOPT% %OCAMLOPTFLAGS% -o showcolors.com %LIB_OPT% showcolors.ml
